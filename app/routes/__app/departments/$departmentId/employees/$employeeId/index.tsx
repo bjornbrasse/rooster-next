c52c229e3cf53bf5774c617057650ee13f347022
Bjorn Brassé <bpbrasse@bra-c.nl>
2022-03-19 02:54:00 +0100

    great progress with useSubmit in task form

--- app/routes/__app/departments/$departmentId/employees/$employeeId/index.tsx
+++ app/routes/__app/departments/$departmentId/employees/$employeeId/index.tsx
@@ -1,5 +1,17 @@
 import * as React from "react";
 
+const Header: React.FC<{ caption: string }> = ({ caption, children }) => {
+  return (
+    <div className="border-b-2 border-sky-500">
+      <p className="ml-1">{children}</p>
+    </div>
+  );
+};
+
 export default function Employee() {
-  return <div>Employee</div>;
+  return (
+    <div className="p-2">
+      <Header caption={""}>Aanwezigheid</Header>
+    </div>
+  );
 }
